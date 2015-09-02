2c722edbfd6985c58eea7b84f6b9c35e8776b250
Eleonore9 <eleonore.mayola@ymail.com>
2015-09-02 15:37:03 +0100

    Add cli options

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
                  [org.clojure/data.csv  "0.1.2"]
                  [clj-http              "2.0.0"]
                  [clj-time              "0.10.0"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/tools.cli "0.3.3"]]
   :plugins [[lein-cljfmt     "0.1.11"]
             [jonase/eastwood "0.2.1"]
-            [lein-kibit      "0.1.2"]])
+            [lein-kibit      "0.1.2"]]
+  :main kixi.hecuba.weather.core)
--- src/kixi/hecuba/weather/core.clj
+++ src/kixi/hecuba/weather/core.clj
@@ -1,6 +1,17 @@
-(ns kixi.hecuba.weather.core)
+(ns kixi.hecuba.weather.core
+  (:require [clj-time.core           :as t]
+            [clj-time.format         :as f]
+            [clj-time.periodic       :as tp]
+            [clojure.tools.cli       :refer [parse-opts]])
+  (:gen-class))
 
+(def cli-options
+  [["-d" "--date DATE" "Date for which you get measurements"
+    :default (f/unparse (f/formatter "dd/MM/YYYY") (t/yesterday))]
+   ["-u" "--user EMAIL" "Email to log into Embed"]
+   ["-p" "--password PASSWORD" "Password to log into Embed"]])
 
 (defn -main [& args]
-  
-)
+  (let [{:keys [date user password] :as opts}
+        (:options (parse-opts  args cli-options))]
+    (str date " - " user " - " password)))
